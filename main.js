1e8249fd1b204a24d96640b1da15db9567d7fb1f
wesley-cantarino <wesley.cantarino@engenharia.ufjf.br>
2019-01-15 11:26:39 -0200

    end :D

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function function_button ()
 {
+  document.getElementById('resul').innerHTML = "";
+  document.getElementById('avi_p').innerHTML = "";
+  document.getElementById('avi_a').innerHTML = "";
+
   var p = document.getElementsByName("peso")[0].value;
   var a = document.getElementsByName("altura")[0].value;
 
@@ -15,6 +19,14 @@ function function_button ()
   else
   {
     var calc = p/(a*a);
+    calc = parseFloat(calc.toFixed(1));
+
     document.getElementById('resul').innerHTML = "Seu imc é " + calc;
+
+    if(p > 300)
+      document.getElementById('avi_p').innerHTML = "o peso está correto?";
+
+    if((a < 0.5)||(a > 2.5))
+      document.getElementById('avi_a').innerHTML = "a altura está correta?";
   }
 }
